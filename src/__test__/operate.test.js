baaa12f539de1eb7468ae292475ddf0907a92551
Hiro Mataba <aganzemataba@gmail.com>
2021-08-19 11:04:06 +0200

    Update test for operate

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -1,19 +1,19 @@
 import operate from '../logic/operate';
 
 describe('Quick Operation', () => {
-  it('should retun two ', () => {
+  it('should retun Two ', () => {
     const plusOperate = operate(1, 1, '+');
     expect(Number(plusOperate)).toBe(2);
   });
-  it('should retun four ', () => {
+  it('should retun Four ', () => {
     const minusOperate = operate(5, 1, '-');
     expect(Number(minusOperate)).toBe(4);
   });
-  it('should retun ten ', () => {
+  it('should retun Ten ', () => {
     const multiplyOperate = operate(5, 2, 'x');
     expect(Number(multiplyOperate)).toBe(10);
   });
-  it('should retun twenty ', () => {
+  it('should retun Twenty ', () => {
     const divideOperate = operate(40, 2, '÷');
     expect(Number(divideOperate)).toBe(20);
   });
